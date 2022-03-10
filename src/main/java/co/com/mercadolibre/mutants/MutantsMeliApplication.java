dcb93c7407da06a2859e389a75d511d114111ba2
velmonto <davemon18@gmail.com>
2022-03-09 21:02:16 -0500

    Creacion de proyecto mutants
    Se crea proyecto para evaluación de cadena de ADN

---
+++ src/main/java/co/com/mercadolibre/mutants/MutantsMeliApplication.java
@@ -0,0 +1,13 @@
+package co.com.mercadolibre.mutants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantsMeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantsMeliApplication.class, args);
+	}
+
+}
