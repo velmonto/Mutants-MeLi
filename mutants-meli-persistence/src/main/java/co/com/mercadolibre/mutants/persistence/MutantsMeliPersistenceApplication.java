2ea3325d047c8f21745b255881f8f4611e25bcf0
velmonto <davemon18@gmail.com>
2022-03-10 22:12:18 -0500

    creacion proyecto mutants meli persistence
    Se crea proyecto mutants meli persistence, encargado de almacenar en la base de datos las evaluaciones de ADN realizadas

---
+++ mutants-meli-persistence/src/main/java/co/com/mercadolibre/mutants/persistence/MutantsMeliPersistenceApplication.java
@@ -0,0 +1,13 @@
+package co.com.mercadolibre.mutants.persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantsMeliPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantsMeliPersistenceApplication.class, args);
+	}
+
+}
---
+++ mutants-meli-persistence/src/test/java/co/com/mercadolibre/mutants/persistence/MutantsMeliPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.mercadolibre.mutants.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantsMeliPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
