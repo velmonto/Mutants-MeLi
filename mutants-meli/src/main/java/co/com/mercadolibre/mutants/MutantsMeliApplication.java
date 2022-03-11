ffd8424dc9416d42925dcaadbc083df7d0ef9afa
velmonto <davemon18@gmail.com>
2022-03-11 17:15:37 -0500

    Eliminacion feing
    Se elimina libreria feing que no estaba siendo usada

--- mutants-meli/src/main/java/co/com/mercadolibre/mutants/MutantsMeliApplication.java
+++ mutants-meli/src/main/java/co/com/mercadolibre/mutants/MutantsMeliApplication.java
@@ -5,7 +5,6 @@ import java.time.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
